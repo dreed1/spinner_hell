5a91f85eebaa24f9e16d8838dcfcf9283542f6ef
Dan Reed <danreedx86@gmail.com>
2014-02-01 23:26:36 -0800

    i blew my whole saturday on this, im proud of myself

--- app/assets/javascripts/hell.js
+++ app/assets/javascripts/hell.js
@@ -11,8 +11,12 @@
     this.context = this.canvas.getContext('2d');
     this.bindEvents();
     this.backgroundColor = '#eee';
-    this.buttonColor = '#dad';
     this.buttonTextColor = '#666';
+
+    this.buttonNormalColor = '#6bd';
+    this.buttonHighlightColor = '#9ce';
+    this.buttonActiveColor = '#acf';
+    this.buttonColor = this.buttonNormalColor;
 
     this.canvasHeight = $(window).height();
     this.canvasWidth = $(window).width();
@@ -23,11 +27,34 @@
       y: this.canvasHeight/2
     };
 
-    this.buttonWidth = 212;
+    this.fire = new Fire({
+      width: this.canvasWidth,
+      height: this.canvasHeight,
+      speed:60,
+      maxPow:12,
+      gravity:0,
+      flameWidth:3,
+      flameHeight:6,
+      fadingFlameSpeed:10
+    });
+
+    this.buttonStartWidth = 212;
+    this.buttonHellWidth = 200;
+    this.buttonWidth = this.buttonStartWidth;
+
+    this.startButtonText = 'TOUCH TO START';
+    this.hellButtonText = 'TOUCH TO STOP';
+    this.buttonText = this.startButtonText;
+
     this.buttonHeight = 44;
     this.buttonOriginX = this.canvasCenter.x - this.buttonWidth/2;
     this.buttonOriginY = this.canvasCenter.y - this.buttonHeight/2;
 
+    this.spinnerSideLength = 200;
+    this.spinnerOuterRadius = 90;
+    this.spinnerInnerRadius = 28;
+    this.spinnerOriginX = this.canvasCenter.x;
+    this.spinnerOriginY = this.canvasCenter.y;
     this.spinnerColor = {
       r: 0,
       g: 0,
@@ -36,6 +63,8 @@
 
     this.spinnerEnabled = false;
     this.inHell = false;
+    this.burning = false;
+    this.buttonVisible = true;
 
     setInterval(function() {
       _this.loop();
@@ -70,33 +99,93 @@
     if (this.checkPointForCollision(mousePosition, buttonRect)) {
       if (this.inHell) {
         this.goToHeaven();
+      } else if (this.inHeaven) {
+
       } else {
         this.spinnerEnabled = true;
+        this.buttonVisible = false;
+        _this.animateSpinnerChange(
+            240, 10, 35
+          )
         setTimeout(function() {
-          _this.inHell = true;
-          _this.backgroundColor = 'black';
-          _this.spinnerColor = {
-            r: 255,
-            g: 255,
-            b: 255
-          }
-          _this.flash(32);
-          $('hell-canvas').fire({
-            mode: 'background',
-            speed:90,
-            maxPow:5,
-            gravity:0,
-            flameWidth:3,
-            flameHeight:80,
-            fadingFlameSpeed:8  
-          });
-        }, 2000);
+          _this.animateSpinnerChange(
+            240, 22, 80
+          )
+          setTimeout(function() {
+            _this.flash(10, "IT'LL NEVER LOAD", function() {
+              _this.goToHell();
+            });
+          }, 4000)
+        }, 4000)
       }
     }
   }
 
+  Simulator.prototype.goToHell = function() {
+    this.buttonVisible = true;
+    this.inHell = true;
+    this.buttonText = this.hellButtonText;
+    this.buttonWidth = this.buttonHellWidth;
+    this.burning = true;
+    this.spinnerEnabled = true;
+    this.spinnerWidth = 500;
+    this.backgroundColor = 'black';
+    this.moveButton();
+    this.spinnerColor = {
+      r: 255,
+      g: 255,
+      b: 255
+    }
+    this.playSong();
+  }
+
+  Simulator.prototype.playSong = function() {
+    $('#video').append('<iframe width="420" height="315" src="//www.youtube.com/embed/LaeIOGFZ0Lc?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+  }
+
+  Simulator.prototype.animateSpinnerChange = function(times, innerRadius, outerRadius) {
+    if (times == 0) return;
+    var _this = this;
+    var newTimes = times - 1;
+    this.spinnerInnerRadius = this.spinnerInnerRadius - (innerRadius / times);
+    this.spinnerOuterRadius = this.spinnerOuterRadius - (outerRadius / times);
+    setTimeout(function() {
+      _this.animateSpinnerChange(newTimes, innerRadius, outerRadius);
+    }, this.frameRate / 1000);
+  }
+
+  Simulator.prototype.changeSpinner = function(innerRadius, outerRadius) {
+    this.spinnerInnerRadius = innerRadius;
+    this.spinnerOuterRadius = outerRadius;
+  }
+
+  Simulator.prototype.goToPurgatory = function() {
+    this.inHell = true;
+    this.burning = true;
+    this.spinnerEnabled = false;
+    this.backgroundColor = '#ddd';
+    this.moveButton();
+    this.spinnerColor = {
+      r: 255,
+      g: 255,
+      b: 255
+    }
+  }
+
   Simulator.prototype.goToHeaven = function() {
-    alert('great job you did it');
+    this.buttonVisible = false;
+    this.inHeaven = true;
+    alert('Great job! You did it, I never expected you to.');
+    this.inHell = false;
+    this.burning = false;
+    this.backgroundColor = 'white';
+    this.spinnerEnabled = false;
+    this.moveButton();
+    this.spinnerColor = {
+      r: 255,
+      g: 255,
+      b: 255
+    }
   }
 
   Simulator.prototype.handleMouseHover = function(mousePosition) {
@@ -107,12 +196,12 @@
       height: this.buttonHeight
     };
     if (this.checkPointForCollision(mousePosition, buttonRect)) {
-      this.buttonColor = "#eac";
+      this.buttonColor = this.buttonHighlightColor;
       if (this.inHell) {
         this.moveButton();
       }
     } else {
-      this.buttonColor = "#dad";
+      this.buttonColor = this.buttonNormalColor;
     }
   }
 
@@ -132,13 +221,13 @@
       colliding = this.checkPointForCollision(mousePosition, buttonRect);
     if (active) {
       if (colliding) {
-        _this.buttonColor = "#cbe";
+        _this.buttonColor = this.buttonActiveColor;
       } 
     } else {
       if (colliding) {
-        _this.buttonColor = "#eac";
+        _this.buttonColor = this.buttonHighlightColor;
       } else {
-        _this.buttonColor = "#dad";
+        _this.buttonColor = this.buttonNormalColor;
       }
     }
   }
@@ -159,19 +248,38 @@
     this.canvasWidth = $(window).width();
     this.canvas.height = this.canvasHeight;
     this.canvas.width = this.canvasWidth;
+    this.fire.width = this.canvasWidth;
+    this.fire.height = this.canvasHeight + 10 + this.fire.flameHeight * 5;
     this.canvasCenter = {
       x: this.canvasWidth/2, 
       y: this.canvasHeight/2
     };
+
+    if (this.burning) {
+      this.updateFire();
+    }
+  }
+
+  Simulator.prototype.updateFire = function() {
+    this.fire.update();
   }
 
   Simulator.prototype.draw = function() {
     //this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     this.drawBackground();
+    if (this.inHell) {
+      this.drawFire();
+      this.drawDerogatoryMessage();
+    }
+    if (this.inHeaven) {
+      this.drawHeaven();
+    }
     if (this.spinnerEnabled) {
       this.drawSpinner();
     }
-    this.drawButton();
+    if (this.buttonVisible) {
+      this.drawButton();
+    }
   }
 
   Simulator.prototype.drawBackground = function() {
@@ -182,26 +290,35 @@
     this.context.fill();
   }  
 
+  Simulator.prototype.drawFire = function() {
+    this.fire.draw();
+  }
+
+  Simulator.prototype.drawDerogatoryMessage = function() {
+    this.context.font = '28pt Calibri';
+    this.context.fillStyle = '#c22';
+    this.context.fillText("WELCOME TO SPINNER HELL", 400, 125);
+  }
+
+  Simulator.prototype.drawHeaven = function() {
+
+  }
+
   Simulator.prototype.drawSpinner = function() {
     var lines = 20,
-        height = Math.min(this.canvasHeight, this.canvasWidth),
-        width = Math.min(this.canvasHeight, this.canvasWidth),
-        originX = this.canvasCenter.x,
-        originY = this.canvasCenter.y,
-        outerRadius = width / 8,
-        innerRadius = width / 2.2,
-        lineWidth = width / 26,
+        lineWidth = this.spinnerSideLength / 26,
         rotation = parseInt(((new Date() - this.timeStart) / 1000) * lines) / lines;
+
     this.context.save();
-    this.context.translate(originX, originY);
+    this.context.translate(this.spinnerOriginX, this.spinnerOriginY);
     this.context.rotate(Math.PI * 2 * rotation);
 
     for (var i = 0; i < lines; i++) {
 
         this.context.beginPath();
         this.context.rotate(Math.PI * 2 / lines);
-        this.context.moveTo(outerRadius, 0);
-        this.context.lineTo(innerRadius, 0);
+        this.context.moveTo(this.spinnerInnerRadius, 0);
+        this.context.lineTo(this.spinnerOuterRadius, 0);
         this.context.lineWidth = lineWidth;
         this.context.strokeStyle = "rgba(" + this.spinnerColor.r + ", " 
                                            + this.spinnerColor.g + ", " 
@@ -229,15 +346,15 @@
     this.context.fill();
     this.context.font = '20pt Calibri';
     this.context.fillStyle = 'black';
-    this.context.fillText('TOUCH TO START', 
+    this.context.fillText(this.buttonText, 
       this.buttonOriginX + 10, 
       this.buttonOriginY + 20 + 10);
   }
 
   Simulator.prototype.writeMessage = function(canvas, message) {
-    this.context.font = '18pt Calibri';
+    this.context.font = '38pt Calibri';
     this.context.fillStyle = 'black';
-    this.context.fillText(message, 10, 25);
+    this.context.fillText(message, 80, 95);
   }
 
   Simulator.prototype.getMousePos = function(canvas, evt) {
@@ -248,16 +365,24 @@
     };
   }
 
-  Simulator.prototype.flash = function(times) {
-    if (times == 0) return;
+  Simulator.prototype.flash = function(times, text, callback) {
+    if (times == 0) {
+      callback();
+      return;
+    }
     var _this = this;
     var nextTime = times - 1;
+
     this.backgroundColor = "white";
     this.spinnerColor = {
       r: 0,
       g: 0,
       b: 0
     }
+    this.context.font = '28pt Calibri';
+    this.context.fillStyle = 'white';
+    this.context.fillText(text, 80, 105);
+
     setTimeout(function() {
       _this.backgroundColor = "black";
       _this.spinnerColor = {
@@ -265,8 +390,11 @@
         g: 255,
         b: 255
       }
+      _this.context.font = '28pt Calibri';
+      _this.context.fillStyle = 'black';
+      _this.context.fillText(text, 80, 105);
       setTimeout(function() {
-        _this.flash(nextTime)
+        _this.flash(nextTime, text, callback);
       },1000/_this.frameRate * 2.3);
     },1000/_this.frameRate * 2.3);
   }
